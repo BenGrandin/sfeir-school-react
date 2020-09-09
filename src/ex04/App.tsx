9201920ba9f1056c8287e28c9ef8ab840fc40a27
Ben_Grandin <bengrandin@hotmail.com>
2020-09-09 14:14:29 +0200

    Ex 4 : Switch between a List view and a Carousel

--- src/ex04/App.tsx
+++ src/ex04/App.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TopAppBarActionItem } from '@rmwc/top-app-bar';
 
 import { Header } from '../solution/Header';
+import { Carousel } from '../ex03/Carousel';
+import { PersonCard } from '../ex02/PersonCard';
 
 type AppProps = {
 	people: People;
 };
 
 export const App: React.FC<AppProps> = ({ people }) => {
+	const [isCarouselView, setIsCarouselView] = useState(false);
+
+
 	return (
 		<>
 			<Header>
-				<TopAppBarActionItem icon="view_carousel" />
-				{/* use "view_module" as icon for showing the list */}
+				<TopAppBarActionItem icon={isCarouselView ? 'view_carousel' : 'view_module'}
+									 onClick={() => {
+										 setIsCarouselView(!isCarouselView);
+										 // ToDo : Is it better cause of async ?
+										 // setIsCarouselView(bool => !bool);
+									 }} />
 			</Header>
+
 			<main>
-				Switch between a List view and a Carousel view to display all the
-				people. Use TopAppBarActionItems in the Header to do so.
-				<br />
 				<br />
 				Omit the manager icon in PersonCard when there is none.
 				<br />
-				<br />
 				Rewrite the Carousel so it has no dependency on PersonCard.
+				<br />
+
+				{
+					isCarouselView ?
+						<Carousel people={people} /> :
+						people.map((p) => <PersonCard key={p.id} randomPerson={p} />)
+				}
 			</main>
 		</>
 	);
