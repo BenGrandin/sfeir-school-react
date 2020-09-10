b93a464737eb6436447da28df0c69ea17e61ea44
Ben_Grandin <bengrandin@hotmail.com>
2020-09-10 11:57:11 +0200

    Ex 5 : Done, Expose an imperative next method on the Carousel in Player

--- src/ex05/Player.tsx
+++ src/ex05/Player.tsx
@@ -1,13 +1,18 @@
-import React, { cloneElement, useState } from 'react';
+import React, { cloneElement, forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import { Fab } from '@rmwc/fab';
 import { range } from '../utils';
 import { PersonCard } from '../solution/PersonCard';
 
+// Todo : Interface vs Type ? Heritage ?
 type CarouselProps = {
 	children: React.ReactElement[];
 };
 
-const Carousel: React.FC<CarouselProps> = ({ children }) => {
+interface CarouselApi {
+	onSkipNext: () => void;
+}
+
+const Carousel = forwardRef<CarouselApi, CarouselProps>(({ children }, ref) => {
 	const childArray = React.Children.toArray(children) as React.ReactElement[];
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const { pred, succ } = range(0, childArray.length - 1);
@@ -17,36 +22,51 @@ const Carousel: React.FC<CarouselProps> = ({ children }) => {
 		[currentIndex, 'current'],
 		[pred(currentIndex), 'prev']
 	];
+	const onSkipPrevious = () => {
+		setCurrentIndex(pred);
+	};
+
+	const onSkipNext = () => {
+		setCurrentIndex(succ);
+	};
+
+	useImperativeHandle(ref, () => ({ onSkipNext }));
 
 	return (
 		<div className="flex-row">
-			<Fab icon="skip_previous" mini onClick={() => setCurrentIndex(pred)} />
+			<Fab icon="skip_previous" mini onClick={onSkipPrevious} />
 			<div className="carousel">
 				{cards.map(([i, className]) =>
 					cloneElement(childArray[i], { className })
 				)}
 			</div>
-			<Fab icon="skip_next" mini onClick={() => setCurrentIndex(succ)} />
+			<Fab icon="skip_next" mini onClick={onSkipNext} />
 		</div>
 	);
-};
+});
 
 type PlayerProps = {
 	people: People;
 };
 
 export const Player: React.FC<PlayerProps> = ({ people }) => {
+	const carouselRef = useRef<CarouselApi>();
+
+	const onFabClick = () => {
+		carouselRef.current.onSkipNext();
+	};
+
 	return (
 		<>
 			<main>
-				<Carousel>
+				<Carousel ref={carouselRef}>
 					{people.map(person => (
 						<PersonCard person={person} key={person.id} />
 					))}
 				</Carousel>
 			</main>
 			<footer>
-				<Fab icon="skip_next" />
+				<Fab onClick={onFabClick} icon="skip_next" />
 			</footer>
 		</>
 	);
