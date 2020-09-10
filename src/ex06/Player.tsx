bf67d72806d0ce040c141d9876d4c9e7a24b7ca6
Ben_Grandin <bengrandin@hotmail.com>
2020-09-10 15:39:14 +0200

    Ex 6 : Done

--- src/ex06/Player.tsx
+++ src/ex06/Player.tsx
@@ -1,32 +1,23 @@
-import React, { cloneElement, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Fab } from '@rmwc/fab';
 import { range } from '../utils';
 import { PersonCard } from '../solution/PersonCard';
 
 type CarouselProps = {
-	children: React.ReactElement[];
+	children?: React.ReactElement[];
+	prev: () => void;
+	next: any;
 };
 
-const Carousel: React.FC<CarouselProps> = ({ children }) => {
+const Carousel: React.FC<CarouselProps> = ({ children, prev, next }) => {
 	const childArray = React.Children.toArray(children) as React.ReactElement[];
-	const [currentIndex, setCurrentIndex] = useState(0);
-	const { pred, succ } = range(0, childArray.length - 1);
-
-	const cards: [number, string][] = [
-		[succ(currentIndex), 'next'],
-		[currentIndex, 'current'],
-		[pred(currentIndex), 'prev']
-	];
-
 	return (
 		<div className="flex-row">
-			<Fab icon="skip_previous" mini onClick={() => setCurrentIndex(pred)} />
+			<Fab icon="skip_previous" mini onClick={prev} />
 			<div className="carousel">
-				{cards.map(([i, className]) =>
-					cloneElement(childArray[i], { className })
-				)}
+				{childArray}
 			</div>
-			<Fab icon="skip_next" mini onClick={() => setCurrentIndex(succ)} />
+			<Fab icon="skip_next" mini onClick={next} />
 		</div>
 	);
 };
@@ -36,18 +27,54 @@ type PlayerProps = {
 };
 
 export const Player: React.FC<PlayerProps> = ({ people }) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [carouselPlaying, setCarouselPlaying] = useState(false);
+	const { pred, succ } = range(0, people.length - 1);
+	const [prev, next] = useMemo(() => {
+		return [
+			() => {
+				setCurrentIndex(pred);
+			},
+			() => {
+				setCurrentIndex(succ);
+			}
+		];
+	}, [people]);
+
+	// Classname should be in carousel.
+	const cards: [number, string][] = [
+		[succ(currentIndex), 'next'],
+		[currentIndex, 'current'],
+		[pred(currentIndex), 'prev']
+	];
+
+	useEffect(() => {
+		if (carouselPlaying) {
+			const carouselInterval = setInterval(next, 2000);
+			return () => {
+				//ToDo : Why can't we return clearInterval directly ?
+				clearInterval(carouselInterval);
+			};
+		}
+		// Todo : Better with dep no?
+	}, [carouselPlaying]);
+
+	const onPlayArrowClick = () => {
+		setCarouselPlaying(play => !play);
+	};
+
 	return (
 		<>
 			<main>
-				<Carousel>
-					{people.map(person => (
-						<PersonCard person={person} key={person.id} />
-					))}
+				<Carousel prev={prev} next={next}>
+					{cards.map(([index, className]) => {
+						const person = people[index];
+						return <PersonCard key={person.id} className={className} person={person} />;
+					})}
 				</Carousel>
 			</main>
 			<footer>
-				<Fab icon="play_arrow" />
-				{/* show the pause icon when playing */}
+				<Fab icon={carouselPlaying ? 'pause' : 'play_arrow'} onClick={onPlayArrowClick} />
 			</footer>
 		</>
 	);
